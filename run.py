68a632c23204340005e24ec3a0cb23f94e3fa5b5
Vasilica Pavaloi <vp.catalin02@yahoo.com>
2022-05-08 08:18:27 +0000

    Initial Commit

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import sys
+import time
+
+#Global Variables
+
+global alien_gun
+alien_gun = "no"
+
