82b24002f7b82b17c0738c05740011228831f64a
Vasilica Pavaloi <vp.catalin02@yahoo.com>
2022-05-08 09:28:20 +0000

    Getting started, creating the first function and introduce the user to the game.

--- run.py
+++ run.py
@@ -6,3 +6,51 @@ import time
 global alien_gun
 alien_gun = "no"
 
+def introdaction():
+    """
+    Display the story line, title and introduce the user into the game.
+    """
+    print("All the strugle and the years past, it all been a success.")
+    print("All for achieving the dream of my life.")
+    time.sleep(3)
+    print("Moom, Dad, I will be an astronaute! I will explore the\n" +
+    "universe and I will see the galaxy!")
+    time.sleep(2)
+    print("It's my time, I need to go, I have a long way ahead\n" +
+    "all the training, and knowledge. Good bye Mom, Dad!")
+    time.sleep(1)
+    print("Vruum, on my way to the space, as Neil Armstrong said\n" + 
+    "\"That's one small step for man, one giant leap for mankind.\"")
+    time.sleep(1)
+    print("Woow... looks exactly as how I've been dreaming about it")
+    time.sleep(2)
+    print(" @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    print(" @@@@@@@@        @@       @@@@@@@@@@@        @@  @@@  @@       @@@@@@@@@@")
+    print(" @@@@@@@@@@@  @@@@@  @@@  @@@@@@@@@@@@@@  @@@@@  @@@  @@  @@@@@@@@@@@@@@@")
+    print(" @@@@@@@@@@@  @@@@@  @@@  @@@@@@@@@@@@@@  @@@@@       @@     @@@@@@@@@@@@")
+    print(" @@@@@@@@@@@  @@@@@  @@@  @@@@@@@@@@@@@@  @@@@@  @@@  @@  @@@@@@@@@@@@@@@")
+    print(" @@@@@@@@@@@  @@@@@       @@@@@@@@@@@@@@  @@@@@  @@@  @@       @@@@@@@@@@")
+    print(" @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    print(" @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    print(" @@@@@@@@@@@@    @@@@@    @@         @@@         @@    @@@@  @@@@@@@@@@@@")
+    print(" @@@@@@@@@@@@  @  @@@  @  @@  @@@@@  @@@  @@@@@  @@  @  @@@  @@@@@@@@@@@@")
+    print(" @@@@@@@@@@@@  @@  @  @@  @@  @@@@@  @@@  @@@@@  @@  @@  @@  @@@@@@@@@@@@")
+    print(" @@@@@@@@@@@@  @@@  @@@@  @@  @@@@@  @@@  @@@@@  @@  @@@  @  @@@@@@@@@@@@")
+    print(" @@@@@@@@@@@@  @@@@@@@@@  @@         @@@         @@  @@@@    @@@@@@@@@@@@")
+    print(" @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    print(" @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    print(" @@        @    @@@@@  @    @@@@@@@@@      @@@        @        @  @@@   @")
+    print(" @@  @@@@  @  @  @@@@  @  @  @@@@@@@@  @@@@  @  @@@@  @  @@@@  @  @@  @@@")
+    print(" @@  @@@@  @  @@  @@@  @  @@  @@@@@@@  @@@@  @  @@@@  @  @@@@@@@  @  @@@@")
+    print(" @@        @  @@@  @@  @  @@@  @@@@@@     @@@@        @  @@@@@@@    @@@@@")
+    print(" @@  @@@@  @  @@@@  @  @  @@@@  @@@@@  @@@@  @  @@@@  @  @@@@@@@  @  @@@@")
+    print(" @@  @@@@  @  @@@@@    @  @@@@@  @@@@  @@@@  @  @@@@  @  @@@@  @  @@  @@@")
+    print(" @@  @@@@  @  @@@@@@   @         @@@@      @@@  @@@@  @        @  @@@   @")
+    print(" @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\n")
+    time.sleep(2)
+    print("Starting to acomodate, the training looks easy, everything seems\n" +
+    "amazing, but you cannot wait for the first lunch, for the first trip\n" + 
+    "for .... the moon!")
+    time.sleep(3)
+    start_game()
+
